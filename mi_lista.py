9564dcc6a879851c26be03cbb556312b452543bc
Angel-Miguel-Valencia-Carrillo <a.22308051280738@cbtis128.edu.mx>
2024-09-09 10:12:38 -0600

    Add files via upload

---
+++ mi_lista.py
@@ -0,0 +1,17 @@
+print("Ejemplo de listas")
+arcoiris=["verde","azul","morado"]
+print(arcoiris)
+longitud=len(arcoiris)
+print("Elemento que contiene la lista arcoiris ", longitud)
+print(f"Elemento que contiene la lista arcoiris v2  {longitud}")
+print("accediendo a un elemento de la lista")
+print(arcoiris[1])
+print(f"elemento en la posicion 0 es {arcoiris[0]}")
+print(f"El primer color del arcoiris es:",arcoiris[0])
+print("agregar a un elemento de la lista")
+print(arcoiris)
+arcoiris.append("naranja")
+print(arcoiris)
+print("Listar los elemntos de la lista ciclo for")
+for Angel in arcoiris:
+    print(Angel)
---
+++ mi_tuplas.py
@@ -0,0 +1,8 @@
+print("Ejemplo de tuplas")
+canciones=("te amo", "El NoaNoa", "amor eterno")
+print(canciones)
+y = list(canciones)
+print(y)
+y[1] = "La Puerta Negra"
+x = tuple(y)
+print(x)
