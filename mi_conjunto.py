14d9eda10da56ee24cdcb079de64893838857cdf
Angel-Miguel-Valencia-Carrillo <a.22308051280738@cbtis128.edu.mx>
2024-09-11 07:49:26 -0600

    Add files via upload

---
+++ mi_conjunto.py
@@ -0,0 +1,7 @@
+print("Ejemplo de conjuntos set")
+tipodeportes={"Futbol", "Tenis", "Basquetball"}
+print(tipodeportes)
+print("Lista de elemntos del conjunto tipodeportes loop for")
+print(tipodeportes)
+for valencia in tipodeportes:
+    print(valencia)
---
+++ mi_diccionario.py
@@ -0,0 +1,9 @@
+print("Ejemplo de diccionario frituras")
+frituras = {
+  "sabor": "queso",
+  "precio": "19.99",
+  "caducidad": 2025
+}
+print(frituras)
+for valencia in frituras:
+  print(valencia)
